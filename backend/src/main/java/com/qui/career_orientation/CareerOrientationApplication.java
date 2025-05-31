8b867cbbb5de435527dfc1dc24bbd14add9cfa68
Qui <quiphanh@gmail.com>
2025-05-31 14:30:41 +0700

    init project

---
+++ backend/src/main/java/com/qui/career_orientation/CareerOrientationApplication.java
@@ -0,0 +1,13 @@
+package com.qui.career_orientation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerOrientationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerOrientationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/qui/career_orientation/CareerOrientationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qui.career_orientation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerOrientationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
